9b49e248bf0c6daaa6793c3adb86e88af9f5f2b9
Yan Martins Esteves <yan.esteves@audaces.com>
2023-07-18 20:17:46 -0300

    Exemplo card info

--- src/components/HomeComponents/CardInfo/CardInfo.jsx
+++ src/components/HomeComponents/CardInfo/CardInfo.jsx
@@ -1,9 +1,14 @@
+import './styles.css'
 function CardInfo({ titulo, valor }) {
     return (
-        <div className="card">
-            <div className="card-body">
-                <h5 className="card-title">{titulo}</h5>
-                <p className="fs-2">{valor}</p>
+        <div className="card-info">
+            <div className="card-body">                
+                <div className="d-flex">
+                    <img src='https://cdn-icons-png.flaticon.com/512/1144/1144760.png'/>
+                    <p className="valor-text">{valor}</p>
+                </div>                
+
+                <h5 className="card-title mt-3">{titulo}</h5>
                 {/* <a href="#" className="card-link">Card link</a>
                 <a href="#" className="card-link">Another link</a> */}
             </div>
